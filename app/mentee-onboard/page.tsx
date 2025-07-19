74f5a31a238e5b95cf70c36393594dbba3b2b0b4
Randila Premarathne <randilamenukapremarathne@gmail.com>
2025-07-19 11:32:29 +0530

    fix git conflicts

--- app/mentee-onboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingForm } from "@/components/mentee-onboard";
-
-export default function MenteeOnboardPage() {
-  return (
-    <main className="container mx-auto px-4 py-8">
-      <OnboardingForm />
-    </main>
-  );
-} 
